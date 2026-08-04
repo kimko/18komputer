29906c371b7df0b0c4e3cef16838630a2351c9d4
Kim Ko <kkopowski@pagerduty.com>
2026-08-04 13:14:12 -0500

    feat: turn app into a Progressive Web App (PWA)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/18komputer/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: '18komputer',
+        short_name: '18komputer',
+        description: '18XX Board Game Assistant',
+        theme_color: '#1a202c', // chakra gray.900
+        background_color: '#1a202c',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     chunkSizeWarningLimit: 1000
   },
